4ec11d2c1145e719bc05bd661825eef2f8a422a3
PranshuSinglaNITD <241210092@nitdelhi.ac.in>
2026-04-28 02:13:26 +0530

    fixed

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    turbopack:false,
-  },
-};
+// const nextConfig: NextConfig = {
+//   experimental: {
+//     turbopack:false,
+//   },
+// };
 
-export default nextConfig;
+// export default nextConfig;
